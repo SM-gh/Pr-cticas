20d556868e432757d384e4c2966d970249b37837
Sebastian <12sebas.mtz@gmail.com>
2018-08-14 21:17:40 -0500

    comentario

---
+++ Perro.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mascota
+{
+    class Perro
+    {
+        //variables globales> Aquellas que no estan dentro de un metodo. Utilizan primer letra en Mayusc.
+        public string Raza;
+        public string Tamaño;
+        public string Sexo;
+        public int Peso;
+        public string Color; //se pone "public" antes de la variable para poder acceder a ellas desde otra clase
+        enum TiposDeComida { CROQUETAS, BASURA, AGUA } // A { Croquetas, Basura, Agua} corresponden los num.> {0,1,2}
+        //Constructor> sirve para inicializar la clase
+        public Perro()
+        {
+            Raza = "French Poodle";
+            Peso = 7;
+            Sexo = "Macho";
+            Tamaño = "Pequeño";
+            Color = "Blanco";
+        }
+        public Perro(string raza, int peso, string sexo, string tamaño, string color)
+        {
+            Raza = raza;
+            Peso = peso;
+            Sexo = sexo;
+            Tamaño = tamaño;
+            Color = color;
+        }
+        //Metodo
+        public void Ladrar() //con el metodo "void"> no da ni recibe información 
+        {
+
+        }
+        public void Correr()
+        {
+
+        }
+        public bool Comer(int alimento) // con este metodo "string"se va a recibir un string
+        {
+            bool resultado = false;
+            if (alimento == (int)TiposDeComida.AGUA)
+            {
+                resultado = false;
+            }
+            else if (alimento == (int)TiposDeComida.BASURA)
+            {
+                resultado = true;
+            }
+            else if(alimento == (int)TiposDeComida.CROQUETAS)
+            {
+                resultado = true;
+            }
+            return resultado;
+        }
+        public void Cagar()
+        {
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mascota
+{
+    public enum TiposDeComida { CROQUETAS, BASURA, AGUA } // A { Croquetas, Basura, Agua} corresponden los num.> {0,1,2}
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool alimentado = false;
+            Perro Perro1 = new Perro(); //"Perro1" es el nombre del objeto de la clase "Perro"
+
+            Perro1.Raza = "French Poodle";
+            Perro1.Peso = 7;
+            Perro1.Sexo = "Macho";
+            Perro1.Tamaño = "Pequeño";
+            Perro1.Color = "Blanco";
+
+            alimentado = Perro1.Comer((int)TiposDeComida.BASURA);
+            if (alimentado)
+            {
+                Console.WriteLine("El perro " + Perro1.Raza + " comió " + Convert.ToString(TiposDeComida.AGUA));
+            }
+            else
+            {
+                Console.WriteLine("El perro ha sido alimentado");
+            }
+            Console.Read();
+
+            Perro Perro2 = new Perro();
+            Console.WriteLine("El perro " + Perro2.Raza + " comió " + Convert.ToString(TiposDeComida.AGUA));
+            Console.Read();
+
+            Perro Perro3 = new Perro("Shitzu",15,"Hembra","Pequeño","Gris");
+            Console.WriteLine("El perro " + Perro3.Raza + " comió " + Convert.ToString(TiposDeComida.AGUA));
+            Console.Read();
+
+        }
+    }
+}
